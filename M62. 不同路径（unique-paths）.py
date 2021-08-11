802d4fb1a1f221ecc165dd81c7d7f97332e47348
fibonacci <zepeng816125@163.com>
2021-08-11 23:26:49 +0800

    add new solution

--- M62. 不同路径（unique-paths）.py
+++ M62. 不同路径（unique-paths）.py
@@ -11,3 +11,16 @@ class Solution:
                 if j > 0:
                     dp[i][j] += dp[i][j-1]
         return dp[m-1][n-1]
+
+    # 动态规划 + 空间压缩
+    # 时间O(mn) 空间O(n)
+    def uniquePaths2(self, m: int, n: int) -> int:
+        dp = [1] * n
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[j] += dp[j-1]
+        return dp[n-1]
+
+    # 计算组合数
+    def uniquePaths3(self, m: int, n: int) -> int:
+        return comb(m + n - 2, n - 1)
